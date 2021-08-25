065178ee4ced88c765081053e4ef591cbd44373c
internetmusic <thor@internetmusic.ltd.uk>
2021-08-25 07:58:58 +0200

    Gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,14 +7,13 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import {StaticQuery, graphql, Link} from "gatsby"
+import {graphql, Link, StaticQuery} from "gatsby"
 
 import Header from "./header"
 import "../../static/styles/main.scss"
 import logo from "../../static/images/NIM-black.svg"
 import iconTwitter from "../../static/images/icon-twitter.svg"
 import iconLinkedin from "../../static/images/icon-linkedin.svg"
-import iconGitHub from "../../static/images/icon-github.svg"
 import iconInstagram from "../../static/images/icon-instagram.svg"
 
 const Layout = ({children}) => (
@@ -54,8 +53,8 @@ const Layout = ({children}) => (
                                     <ul>
                                         <li><Link to="/integrations/slack" title={"Integrations"}>Integrations</Link></li>
                                         <li><Link to="/about" title={"About Us"}>About</Link></li>
-                                        <li><a href={"https://histaff.io/blog"}>Blog</a></li>
-                                        <li><a className={"links__special"} href={"https://feedback.histaff.io/"} target={"_blank"} title={"We look forward to receiving your great feedback"}>Feedback</a></li>
+                                        <li><a href={"https://copyrightcoinsnews.com"}>Newsroom</a></li>
+                                        <li><a className={"links__special"} href={"https://copyrightcoinsnews.com/newsletter-signup/"} target={"_blank"} title={"Please sign up for our newsletters"}>Feedback</a></li>
                                     </ul>
                                 </ul>
                             </div>
@@ -77,10 +76,9 @@ const Layout = ({children}) => (
                         <div className={"col-3"}>
                             <div className={"widget__item"}>
                                 <div className={"social"}>
-                                    <a href="https://twitter.com/histaffio" target={"_blank"} title={"Twitter"}><img alt={"Twitter"} src={iconTwitter}/></a>
-                                    <a href="https://www.linkedin.com/company/histaff/" target={"_blank"} title={"LinkedIn"}><img alt={"LinkedIn"} src={iconLinkedin}/></a>
-                                    <a href="https://github.com/histaff" target={"_blank"} title={"GitHub"}><img alt={"GitHub"} src={iconGitHub}/></a>
-                                    <a href="https://www.instagram.com/histaff.io/" target={"_blank"} title={"Instagram"}><img alt={"Instagram"} src={iconInstagram}/></a>
+                                    <a href="https://twitter.com/copyrightcoins" target={"_blank"} title={"Twitter"}><img alt={"Twitter"} src={iconTwitter}/></a>
+                                    <a href="https://www.linkedin.com/company/internetmusicltd" target={"_blank"} title={"LinkedIn"}><img alt={"LinkedIn"} src={iconLinkedin}/></a>
+                                    <a href="https://www.instagram.com/copyrightcoins/" target={"_blank"} title={"Instagram"}><img alt={"Instagram"} src={iconInstagram}/></a>
                                 </div>
                             </div>
                         </div>
