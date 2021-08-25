d8b341ebb3dcfbd5fd391d2cf80415c4c5ad7b16
internetmusic <thor@internetmusic.ltd.uk>
2021-08-25 10:26:09 +0200

    Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,6 @@ const IndexPage = () => (
                 The essence of the ecosystem is to enable creative people everywhere to live upon their creativity.<br/>
             </p>
             <img alt={"Dashboard"} src={featureImage}/>
-            <img alt={"Dashboard"} src="https://vimeo.com/543209885"/>
         </div>
 
         <div className={"container"}>
@@ -35,8 +34,12 @@ const IndexPage = () => (
 
                         <div className={"col-6"}>
                             <div className={"feature__content"}>
-                                <h2>Create and Join Events</h2>
-                                <p>Save your time and energy by letting HiStaff to manage your events (handle the colleague’s birthday, gathering, etc.)</p>
+                                <h2>NIM's CopyrightChain</h2>
+                                <p>The NIM roads are based upon the NIM CopyrightChains infrastructure that is a spoon (friendly fork) of the Avalanche blockchain.
+                                    There are many types of roads leading into the main road (CopyrightChains).
+                                    Each has its own purpose and  its own access control. Most of the roads are free to use, and some are out of necessity at a basic cost  in order to maintain optimal performance .
+                                    In addition, the NIM ecosystem includes independent elements that  consist of a grouping of services that interact with each other and the world around them to create an environment with very special features.
+                                </p>
                             </div>
                         </div>
                     </div>
