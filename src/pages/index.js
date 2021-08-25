22851736b3b86eee53b8d89f4d385d5d2bab5f14
internetmusic <thor@internetmusic.ltd.uk>
2021-08-25 10:27:49 +0200

    Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import featureImage from "../../static/images/OneClick.gif";
-import thumbnailEvent from "../../static/images/feature-event.png"
+import thumbnailEvent from "../../static/images/CopyrightCoins.gif"
 import thumbnailBoard from "../../static/images/feature-board.png"
 import thumbnailNews from "../../static/images/feature-news.png"
 import thumbnailTeams from "../../static/images/feature-team.png"
